f1f1edc1871213bcb01857c06d39ea21e6ac4703
so23cha <sion.23.cha@gmail.com>
2025-06-25 17:00:56 +0900

    feat: update About page text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
       {/* 로고 + 텍스트 */}
       <div className="flex-1 flex items-center space-x-2">
         <Image
-          src="/images/logo.png"      // public/images/logo.png
+          src="/images/logo.jpg"      // public/images/logo.png
           alt="REALIVA AI Logo"
           width={32}
           height={32}
